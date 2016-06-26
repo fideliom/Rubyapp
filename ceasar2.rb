691796eb7b7e61a28ca5ffd2bcb531a9296d5aa9
fideliom <jlndbrh@gmail.com>
2016-06-26 01:27:24 -0700

    update

---
+++ ceasar2.rb
@@ -0,0 +1,38 @@
+def special(char)
+	if (char == ' ' || char == '?' || char == '!' || char == '%' || char == 
+	'&')
+	return true
+	else
+	return false
+	end
+end
+
+def ceasar(string, key)
+	key = key % 26
+	str = ''
+	string.each_char do |c|
+		if (special(c) == false)
+		key.times do 
+			c.next!
+		end
+		if (c.length > 1)
+		c = c[-1]
+		end
+		end
+		str += c
+	end
+	return str
+end
+
+def encrypt
+puts "Please enter your text:"
+text = gets.chomp
+puts "Please enter your key:"
+key = gets.chomp
+key = key.to_i
+puts "Your text encrypted with the ceasar cypher:"
+puts "Using the key #{key} :"
+puts ceasar(text, key)
+end
+
+encrypt
